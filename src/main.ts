eb54332c564a1bdcbbbe0b9469d06fd79b3e42a9
nitesh-dev <niteshdev547@gmail.com>
2023-07-13 23:09:44 +0530

    created app

--- src/main.ts
+++ src/main.ts
@@ -3,167 +3,8 @@ import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { mod } from 'three/examples/jsm/nodes/Nodes.js';
 
 
-// setting up scene
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.useLegacyLights = false;
-document.body.appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 0.5, 0);
-
-// controls.enablePan = false;
-// controls.enableDamping = true;
-
-
-let modelLoader = new GLTFLoader();
-let model = await modelLoader.loadAsync('./scene.gltf');
-
-let group = new THREE.Group()
-
-
-// adding lightning
-var ambientLight = new THREE.AmbientLight(0xffffff, 3); // Soft white light
-scene.add(ambientLight);
-
-
-// loading texture
-var textureLoader = new THREE.TextureLoader();
-let earthDiffusedTexture = await textureLoader.loadAsync('./earth-diffused-texture.jpg');
-let earthBumpTexture = await textureLoader.loadAsync('./earth-bump-texture.jpg');
-let earthSpecularTexture = await textureLoader.loadAsync('./earth-specular-texture.jpg');
-
-
-
-function createEarth() {
-    // setting up earth model
-    var geometry = new THREE.SphereGeometry(2, 32, 32);
-    var material = new THREE.MeshPhongMaterial({ map: earthDiffusedTexture, bumpMap: earthBumpTexture, specularMap: earthSpecularTexture });
-    // material.bumpScale = 0.01
-    // material.specular = new THREE.Color('white')
-
-    var earthMesh = new THREE.Mesh(geometry, material);
-    // adding rotation offset
-    return earthMesh
-}
-
-
-// function posToCircle(pos: THREE.Vector3) {
-//     //ball
-//     const geometry = new THREE.SphereGeometry(0.2);
-//     geometry.translate(pos.x, pos.y, pos.z)
-//     const material = new THREE.MeshBasicMaterial({ color: 0xffaabb });
-//     const cube = new THREE.Mesh(geometry, material);
-//     group.add(cube);
-
-//     //circle
-//     const len = pos.length()
-//     const circleGeometry = new THREE.BufferGeometry().setFromPoints(
-//         new THREE.Path().absarc(0, 0, len, 0, Math.PI * 2, true).getSpacedPoints(20)
-//     );
-//     const circleMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
-//     let quaternion = new THREE.Quaternion();
-//     quaternion = quaternion.setFromUnitVectors(new THREE.Vector3(1, 0, 0).normalize(), pos.normalize())
-//     circleGeometry.applyQuaternion(quaternion)
-//     const circle = new THREE.Line(circleGeometry, circleMaterial);
-//     group.add(circle);
-// }
-
-
-function createSatellitePath(latitude: number, longitude: number, showLine = true) {
-
-    // let coordinate = convertTo3DCoordinates(20.5937, 78.9629, 2.5)
-    let coordinate = convertTo3DCoordinates(latitude, longitude, 2.5)
-
-    createSatellite(coordinate)
-
-    // Create the circular path
-    const circleGeometry = new THREE.BufferGeometry().setFromPoints(
-        new THREE.Path().absarc(0, 0, 2.5, 0, Math.PI * 2, true).getSpacedPoints(50)
-    );
-
-    const circleMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
-
-    let quaternion = new THREE.Quaternion();
-    quaternion = quaternion.setFromUnitVectors(new THREE.Vector3(1, 0, 0).normalize(), coordinate.clone().normalize())
-    circleGeometry.applyQuaternion(quaternion)
-    const circle = new THREE.Line(circleGeometry, circleMaterial);
-
-
-
-    // showing line
-    if (showLine) {
-        const points = [];
-        points.push(coordinate);
-        points.push(new THREE.Vector3(0, 0, 0));
-
-        const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-        const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-        const line = new THREE.Line(lineGeometry, material);
-
-        return [circle, line]
-    } else {
-        return [circle]
-    }
-
-}
-
-
-group.add(createEarth())
-
-createSatellitePath(-27.440049, 135.427246).forEach(element => {
-    group.add(element)
-});
-
-createSatellitePath(-5.440049, 135.427246).forEach(element => {
-    group.add(element)
-});
-
-createSatellitePath(50.440049, 135.427246).forEach(element => {
-    group.add(element)
-});
-
-createSatellitePath(-5.440049, 30.427246).forEach(element => {
-    group.add(element)
-});
-
-createSatellitePath(-50.440049, 135.427246).forEach(element => {
-    group.add(element)
-});
-
-createSatellitePath(20.5937, 78.9629).forEach(element => {
-    group.add(element)
-});
-
-
-
-
-function createSatellite(pos: THREE.Vector3) {
-    let modelClone = model.scene.clone()
-    modelClone.position.set(pos.x, pos.y, pos.z)
-    modelClone.scale.set(0.02, 0.02, 0.02)
-
-    // Calculate the direction vector towards the origin
-    var direction = new THREE.Vector3().subVectors(scene.position, modelClone.position).normalize();
-
-    // Calculate the quaternion representing the rotation towards the origin
-    var quaternion = new THREE.Quaternion().setFromUnitVectors(modelClone.up, direction);
-
-    // Apply the rotation to the model scene
-    modelClone.setRotationFromQuaternion(quaternion);
-
-    group.add(modelClone)
-}
-
-
-
-scene.add(group)
 
 
 function convertTo3DCoordinates(latitude: number, longitude: number, radius: number) {
@@ -177,28 +18,323 @@ function convertTo3DCoordinates(latitude: number, longitude: number, radius: num
     return new THREE.Vector3(x, y, z);
 }
 
+class SatelliteObject {
+    latitude = 0
+    longitude = 0
+    id = 0
+    selected = false
+
+    modelClone = this.createSatellite(new THREE.Vector3())
+    paths: THREE.Line<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.LineBasicMaterial>[] | null = null
+
+    constructor(latitude: number, longitude: number, id: number, selected = false) {
+        this.latitude = latitude
+        this.longitude = longitude
+        this.id = id
+        this.selected = selected
+
+        this.update(this.latitude, this.longitude)
+
+        // adding to scene
+        group.add(this.modelClone)
+        console.log(this.modelClone)
+
+    }
 
 
-// Render loop
+    update(latitude: number, longitude: number) {
+
+        // convert lon and lat to coordinate
+        let coordinate = convertTo3DCoordinates(latitude, longitude, 2.5)
+
+        // setting position and rotation
+        this.modelClone.position.set(coordinate.x, coordinate.y, coordinate.z)
+
+        // Calculate the direction vector towards the origin
+        var direction = new THREE.Vector3().subVectors(scene.position, this.modelClone.position).normalize();
+
+        // Calculate the quaternion representing the rotation towards the origin
+        var quaternion = new THREE.Quaternion().setFromUnitVectors(this.modelClone.up, direction);
+
+        // Apply the rotation to the model scene
+        this.modelClone.setRotationFromQuaternion(quaternion);
+
+
+
+        // creating paths
+        if (this.paths != null) {
+
+            // delete the previous path
+            group.remove(this.paths[0])
+            group.remove(this.paths[1])
+
+            // create the path again
+            this.paths = this.createSatellitePath(coordinate, this.selected)
+        } else {
+            this.paths = this.createSatellitePath(coordinate, this.selected)
+        }
+    }
+
+    private createSatellitePath(coordinate: THREE.Vector3, selected = false) {
+
+        // Create the circular path
+        const circleGeometry = new THREE.BufferGeometry().setFromPoints(
+            new THREE.Path().absarc(0, 0, 2.5, 0, Math.PI * 2, true).getSpacedPoints(50)
+        );
+
+        // creating material
+        let circleMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+        if (selected) circleMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+
+        let quaternion = new THREE.Quaternion();
+        quaternion = quaternion.setFromUnitVectors(new THREE.Vector3(1, 0, 0).normalize(), coordinate.clone().normalize())
+        circleGeometry.applyQuaternion(quaternion)
+        const circle = new THREE.Line(circleGeometry, circleMaterial);
+
+
+        // showing line
+        const points = [];
+        points.push(coordinate);
+        points.push(new THREE.Vector3(0, 0, 0));
+
+        const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+        const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+        const line = new THREE.Line(lineGeometry, material);
+
+
+        // adding to scene
+        group.add(circle)
+        group.add(line)
+
+        return [circle, line]
+    }
+
+    private createSatellite(pos: THREE.Vector3) {
+        let modelClone = model.scene.clone()
+
+        modelClone.name = 'satellite'
+        modelClone.position.set(pos.x, pos.y, pos.z)
+        modelClone.scale.set(0.02, 0.02, 0.02)
+
+        return modelClone
+    }
+
+    remove() {
+
+        // removing all items
+        group.remove(this.modelClone)
+        if (this.paths != null) {
+            group.remove(this.paths[0])
+            group.remove(this.paths[1])
+        }
+    }
+
+}
+
+class App {
+    renderer: THREE.WebGLRenderer;
+
+    camera: THREE.PerspectiveCamera;
+    controls: OrbitControls;
+    modelLoader: GLTFLoader;
+    raycaster: THREE.Raycaster;
+
+    satelliteObjects = Array<SatelliteObject>()
+
+    constructor(container: HTMLDivElement) {
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.useLegacyLights = false;
+        container.appendChild(this.renderer.domElement);
+
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.target.set(0, 0.5, 0);
+
+        this.modelLoader = new GLTFLoader();
+
+        this.raycaster = new THREE.Raycaster();
+
+        var ambientLight = new THREE.AmbientLight(0xffffff, 3); // Soft white light
+        scene.add(ambientLight);
+        scene.add(group)
+        this.init()
+
+    }
+    protected init() {
+        this.renderer.domElement.addEventListener('click', (e) => {
+            const mouse = new THREE.Vector2();
+            mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+            mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+
+            console.log(this.renderer.domElement.clientWidth, this.renderer.domElement.clientHeight)
+
+            this.checkIntersection(mouse);
+        });
+
+        //meshes and models
+        group.add(this.createEarth())
+
+        //temp
+        let temp = Array<{ id: number, latitude: number, longitude: number, selected: boolean }>()
+        temp.push({
+            id: 0,
+            latitude: -27.440049,
+            longitude: 135.427246,
+            selected: false
+        })
+
+        temp.push({
+            id: 1,
+            latitude: -5.440049,
+            longitude: 100,
+            selected: false
+        })
+
+
+        temp.push({
+            id: 2,
+            latitude: 50,
+            longitude: 100,
+            selected: false
+        })
+
+
+        temp.push({
+            id: 3,
+            latitude: -50,
+            longitude: 100,
+            selected: false
+        })
+
+
+
+        this.updateObjects(temp)
+
+    }
+
+    private checkIntersection(mouse:THREE.Vector2) {
+
+        this.raycaster.setFromCamera(mouse, this.camera);
+
+        const intersects = this.raycaster.intersectObject(group, true);
+
+        if (intersects.length > 0) {
+
+            const selectedObject = intersects[0].object;
+            console.log(selectedObject)
+            //selectedObject.material.color.set('#69f');
+
+        } else {
+
+            // outlinePass.selectedObjects = [];
+
+        }
+
+    }
+    animate() {
+        this.controls.update();
+
+        this.renderer.render(scene, this.camera);
+    }
+    resize() {
+
+    }
+    private updateObjects(objects: Array<{ id: number, latitude: number, longitude: number, selected: boolean }>) {
+
+        let matchedObject = Array<SatelliteObject>()
+
+        // updating previous objects
+        this.satelliteObjects.forEach(item => {
+            const index = this.findSameObject(item.id, objects)
+
+            // if match found
+            if (index != -1) {
+
+                // update the object if coordinate changes or selected changes
+                if (item.latitude != objects[index].latitude || item.longitude != objects[index].longitude || item.selected != objects[index].selected) {
+
+                    item.selected = objects[index].selected
+                    item.update(objects[index].latitude, objects[index].longitude)
+                }
+
+                matchedObject.push(item)
+
+                // removing the matched object from search
+                objects.splice(index, 1)
+
+            } else {
+
+                // remove the object from render
+                item.remove()
+            }
+        });
+
+
+        // creating the remaining elements
+        objects.forEach(item => {
+            const obj = new SatelliteObject(item.latitude, item.longitude, item.id, item.selected)
+            matchedObject.push(obj)
+        });
+
+        this.satelliteObjects = matchedObject
+    }
+    private findSameObject(id: number, objects: Array<{ id: number, latitude: number, longitude: number, selected: boolean }>) {
+        for (let index = 0; index < objects.length; index++) {
+
+            if (objects[index].id == id) {
+                return index
+            }
+
+        }
+
+        return -1
+    }
+    private createEarth() {
+        // setting up earth model
+        var geometry = new THREE.SphereGeometry(2, 32, 32);
+        var material = new THREE.MeshPhongMaterial({ map: earthDiffusedTexture, bumpMap: earthBumpTexture, specularMap: earthSpecularTexture });
+        // material.bumpScale = 0.01
+        // material.specular = new THREE.Color('white')
+
+        var earthMesh = new THREE.Mesh(geometry, material);
+        // adding rotation offset
+        return earthMesh
+    }
+
+}
+
+let modelLoader = new GLTFLoader();
+let model = await modelLoader.loadAsync('./scene.gltf');
+
+var textureLoader = new THREE.TextureLoader();
+let earthDiffusedTexture = await textureLoader.loadAsync('./earth-diffused-texture.jpg');
+let earthBumpTexture = await textureLoader.loadAsync('./earth-bump-texture.jpg');
+let earthSpecularTexture = await textureLoader.loadAsync('./earth-specular-texture.jpg');
+
+var scene = new THREE.Scene();
+var group = new THREE.Group()
+
+const app = new App(document.querySelector('#app') as HTMLDivElement);
 
 let previousTimestamp = 0;
 const earthSpeed = 0.1;
-camera.position.z = 10
+app.camera.position.z = 10
 
 function animate(timestamp: number) {
-    controls.update();
     // calculating delta time in second, because requestAnimationFrame will give time in milliseconds
     const delta = (timestamp - previousTimestamp) / 1000
     previousTimestamp = timestamp
 
     group.rotation.y += earthSpeed * delta;
 
-
+    app.animate()
     // rerender the scene
-    renderer.render(scene, camera);
 
     requestAnimationFrame(animate);
 }
 
-// Start the animation loop
+// // Start the animation loop
 requestAnimationFrame(animate);
+
+
